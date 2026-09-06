56d64dc21f4eeea94ab7a4ad36f08b524b6a69df
cg2014a <cgarrett4@hotmail.com>
2026-09-06 11:43:34 -0500

    Add person choice controls

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'spin-dine-v1-002';
+const CACHE_NAME = 'spin-dine-v1-003';
 const APP_SHELL = ['./', './index.html', './style.css', './wheel.css', './app.js', './manifest.webmanifest', './icons/spin-dine-icon.svg'];
 
 self.addEventListener('install', (event) => {
